45a61805c9c301a4234952355a0c32c18233fc10
Fernando <159575977+fernandoaxis@users.noreply.github.com>
2024-03-21 16:34:10 -0300

    Add files via upload

--- src/calculadora/calculadoraNC.java
+++ src/calculadora/calculadoraNC.java
@@ -1,26 +1,48 @@
 package calculadora;
 
+import dominio.PolarConv;
 import dominio.RetangularConv;
 import java.util.Scanner;
 
 public class calculadoraNC {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    	 Scanner scanner = new Scanner(System.in);
+         System.out.println("01 - Polar para Retangular | 02 - Retangular para Polar");
 
-        System.out.print("Digite o valor de A: ");
-        double valorA = scanner.nextDouble();
-        System.out.print("Digite o valor de B: ");
-        double valorB = scanner.nextDouble();
+         int opcao = scanner.nextInt();
+         if(opcao == 1) { 
+             System.out.print("Digite a magnitude (r): ");
+             double polarMagnitude = scanner.nextDouble();
+             System.out.print("Digite o ângulo em graus (θ): ");
+             double polarAngulo = scanner.nextDouble();
 
-        RetangularConv conv = new RetangularConv(valorA, valorB);
+             PolarConv polar = new PolarConv(polarMagnitude, polarAngulo);
+             double[] coordenadaRetangular = polar.toRectangular();
 
-        System.out.println("Iniciando a conversão...\n");
+             System.out.println("Coordenadas retangulares: ");
+             System.out.println("x: " + coordenadaRetangular[0]);
+             System.out.println("y: " + coordenadaRetangular[1]);
+         } else if(opcao == 2) { 
+             System.out.print("Digite o valor de A: ");
+             double valorA = scanner.nextDouble();
+             System.out.print("Digite o valor de B: ");
+             double valorB = scanner.nextDouble();
 
-        conv.calcularAmplitude();
-        conv.calcularAngulo();
+             RetangularConv conv = new RetangularConv(valorA, valorB);
+             System.out.println("Iniciando a conversão...\n");
 
-        System.out.println("Z = " + conv.getAmplitude() + "<" + conv.getAngulo() + " graus");
+             conv.calcularAmplitude();
+             conv.calcularAngulo();
 
-        scanner.close();
+             System.out.println("Z = " + conv.getAmplitude() + "<" + Math.toDegrees(conv.getAngulo()) + " graus");
+         } else {
+             System.out.println("Opção inválida");
+         }
+
+         scanner.close();
+    	
+    	
+    	
+
     }
 }
--- src/dominio/PolarConv.java
+++ src/dominio/PolarConv.java
@@ -1,5 +1,32 @@
 package dominio;
 
 public class PolarConv {
+	
+	   private double magnitude;
+	    private double anguloGrau;
 
-}
+	    public PolarConv(double magnitude, double angulograu) {
+	        this.magnitude = magnitude;
+	        this.anguloGrau = angulograu;
+	    }
+
+	    public double[] toRectangular() {
+	        double angleInRadians = Math.toRadians(anguloGrau);
+	        double x = magnitude * Math.cos(angleInRadians);
+	        double y = magnitude * Math.sin(angleInRadians);
+	        
+	        return new double[]{x, y};
+	    }
+
+	    public static void main(String[] args) {
+	        PolarConv polarCoord = new PolarConv(5.0, 45.0); 
+	        
+	        double[] rectangularCoord = polarCoord.toRectangular(); 
+	        
+	        System.out.println("Coordenadas retangulares: ");
+	        System.out.println("x: " + rectangularCoord[0]);
+	        System.out.println("y: " + rectangularCoord[1]);
+	    }
+	    
+	}
+	
